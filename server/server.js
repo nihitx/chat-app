9d004154f24565741da50aacb7fcf2c33c5d614e
nihitx <masnadhossain@live.com>
2017-01-25 21:20:19 +0200

    create generateMessage utility and added mocha for testing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-
+//Es6 destructuring
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -16,27 +17,15 @@ io.on('connection', (socket)=>{
   console.log('New user connected');
 
 // here the new message is shown to the user
-  socket.emit('newMessage',{
-    from : 'Admin',
-    text : 'Welcome to chat app',
-    createdAt : new Date().getTime()
-  });
+  socket.emit('newMessage',generateMessage('Admin', 'Welcome to the chat app'));
 // here the message tells the other user that someone joined
-  socket.broadcast.emit('newMessage',{
-    from : 'Admin',
-    text : 'new user joined',
-    createdAt : new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 // this part recives
 //the create message from client and sends off a new message
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
-    io.emit('newMessage',{
-      from : message.from,
-      text : message.text,
-      createdAt : new Date().getTime()
-    });
+    io.emit('newMessage',generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', ()=>{
