ae6275d1ef8e9314d3af8c498a6fc36ef87ea05c
nihitx <masnadhossain@live.com>
2017-02-01 20:49:27 +0200

    added form for messages and added jquery to show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -12,11 +12,24 @@ socket.on('disconnect', function(){
 //ricieves the newMessage and shows all the users
 socket.on('newMessage', function(message){
     console.log('newMessage', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
 });
 
-socket.emit('createMessage',{
-  from : 'Masnad',
-  text : 'Server did you get this?'
-},function(data){
-  console.log(data);
-});;
+// socket.emit('createMessage',{
+//   from : 'Masnad',
+//   text : 'Server did you get this?'
+// },function(data){
+//   console.log(data);
+// });;
+
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+  socket.emit('createMessage',{
+    from : 'User',
+    text : $('[name=message]').val()
+  },function(){
+
+  });;
+});
